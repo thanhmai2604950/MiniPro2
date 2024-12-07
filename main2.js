4a8111ac61eac29edb5113403d32ba5dffa72874
thanhmai2604950 <rosie052606@gmail.com>
2024-12-07 15:35:25 +0700

    Câu 2

---
+++ main2.js
@@ -0,0 +1,14 @@
+const character = {
+  Name: 'Maleficent',
+  age: 1000,
+  role: 'fairy',
+  planet: 'Moors',
+}
+
+const {Name, age, role, planet} = character;
+console.log('Name:'+' '+character.Name);
+console.log('Age:'+' '+character.age);
+console.log('Role:'+' '+character.role);
+console.log('Planet:'+' '+character.planet);
+
+  
